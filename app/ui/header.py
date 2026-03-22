5bd150ad50715209ccdebcf38d9e8d72de5728f5
sachinomniebuzz <skohli-contractor@buzzintegrations.com>
2026-03-22 13:25:06 +0530

    test auto deploy

--- app/ui/header.py
+++ app/ui/header.py
@@ -4,6 +4,7 @@ def render_header():
     with gr.Row(elem_classes="header"):
         gr.Markdown("## NAVES")
         with gr.Row(elem_classes="nav"):
+            gr.Markdown("## 🚀 NAVES CI/CD LIVE SUCCESS")
             gr.Button("Home")
             gr.Button("Men")
             gr.Button("Women")
